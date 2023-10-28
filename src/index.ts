4c328d99794b98fd007d57175f2ca0eccdb6f33c
Rafael <rafa.dev.oliveira@gmail.com>
2023-10-28 00:27:41 +0000

    Challenge

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import { BankAccount  } from "./classes/BankAccount";
 
 const ba1 = new BankAccount("Rafael", "312hg3ihj12687312", "1231231267312");
+
+
+// Criar uma classe chamada transaction
+// A classe, guarda o valor da transação
+// um ID de conta destinatário
+// Guarda o ID do remetente
+// Também assim que criada, guarda a data atual exata, do
+// Momento de criação
+// Os campos devem ser
+// idRemetente
+// idDestinatario
+// valor
+// dataDeCriacao
